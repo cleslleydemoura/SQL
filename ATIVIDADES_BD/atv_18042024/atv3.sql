4fafc618e8c7722a149f3f895bf94953d5052d86
Cleslley de Moura <100368699+cleslleydemoura@users.noreply.github.com>
2024-04-25 10:10:30 -0300

    Update atv3.sql

--- ATIVIDADES_BD/atv_18042024/atv3.sql
+++ ATIVIDADES_BD/atv_18042024/atv3.sql
@@ -16,3 +16,22 @@ CREATE TABLE cursinho.curriculo(
 	foreign key (cod_discp) REFERENCES cursinho.disciplina (cod_discp),
 	foreign key (cod_curso) REFERENCES cursinho.curso (cod_curso)
 );
+
+INSERT INTO cursinho.disciplina (cod_discp, nome) VALUES (1, 'Cálculo I');
+INSERT INTO cursinho.disciplina (cod_discp, nome) VALUES (2, 'POO');
+INSERT INTO cursinho.disciplina (cod_discp, nome) VALUES (3, 'BD');
+INSERT INTO cursinho.disciplina (cod_discp, nome) VALUES (4, 'Cobol');
+
+INSERT INTO cursinho.curso (cod_curso, nome) VALUES (10, 'ccp');
+INSERT INTO cursinho.curso (cod_curso, nome) VALUES (20, 'ads');
+INSERT INTO cursinho.curso (cod_curso, nome) VALUES (30, 'eds');
+INSERT INTO cursinho.curso (cod_curso, nome) VALUES (40, 'eng');
+
+INSERT INTO cursinho.curriculo (obrigatoriedade, codd, codc) VALUES ('sim', 1, 10);
+INSERT INTO cursinho.curriculo (obrigatoriedade, codd, codc) VALUES ('sim', 2, 20);
+INSERT INTO cursinho.curriculo (obrigatoriedade, codd, codc) VALUES ('sim', 3, 30);
+INSERT INTO cursinho.curriculo (obrigatoriedade, codd, codc) VALUES ('não', 4, 40);
+
+SELECT * FROM cursinho.disciplina
+SELECT * FROM cursinho.curso
+SELECT * FROM cursinho.curriculo
