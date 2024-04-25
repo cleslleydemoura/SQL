d4041f0f5a709bb78e69654cf19387743695cb96
Cleslley de Moura <100368699+cleslleydemoura@users.noreply.github.com>
2024-04-25 10:51:02 -0300

    Update atv3.sql

--- ATIVIDADES_BD/atv_18042024/atv3.sql
+++ ATIVIDADES_BD/atv_18042024/atv3.sql
@@ -35,3 +35,38 @@ INSERT INTO cursinho.curriculo (obrigatoriedade, codd, codc) VALUES ('não', 4, 
 SELECT * FROM cursinho.disciplina
 SELECT * FROM cursinho.curso
 SELECT * FROM cursinho.curriculo
+
+-- 1) Selecione todos os nomes das disciplinas
+SELECT nome FROM cursinho.disciplina;
+SELECT * FROM cursinho.disciplina;
+-- 2) Selecione todos oc cursos
+SELECT nome FROM cursinho.curso;
+SELECT * FROM cursinho.curso;
+-- 3) Inserir um novo curso, cujo o código é igual a 321
+INSERT INTO cursinho.curso(cod_curso) VALUES (321);
+-- 4) Selecione o nome do curso cujo o código é igual a 321
+SELECT nome FROM cursinho.curso WHERE cod_curso=321;
+-- 5) Selecione todos os nomes de disciplinas que comecem com a letra B
+SELECT nome FROM cursinho.disciplina WHERE nome LIKE 'B%' 
+	-- '%' ao final significa que vai procurar o que COMEÇA com B
+	-- '%' ao final ele vai procurar as variáveis que TERMINAM em B.
+-- 6) Adicione um novo atributo em disciplina, valor (numeric)
+ALTER TABLE cursinho.disciplina ADD valor numeric(50,1);
+-- 7) Adicionar valores para todas as disciplinas cadastradas
+UPDATE cursinho.disciplina SET valor=45;
+UPDATE cursinho.disciplina SET valor=45 WHERE cod_discp = 45 AND nome = 'LFA';
+
+SELECT * FROM cursinho.disciplina;
+-- 8) Liste a média dos valores de todas as disciplinas
+SELECT AVG(valor) AS media FROM cursinho.disciplina;
+-- 9) Liste o código das disciplinas 'BD' e 'IA'
+SELECT cod_discp FROM cursinho.disciplina WHERE nome = 'POO' OR nome = 'Cobol'
+
+-- 10) Liste o nome dos cursos que tenham a disciplina cujo o código é 2
+SELECT c.nome FROM cursinho.disciplina d, cursinho.curso c;
+SELECT c.nome
+FROM cursinho.disciplina d, cursinho.curso c, cursinho.curriculo w
+WHERE w.codc=c.cod_curso 
+AND w.codd=d.cod_discp
+AND d.cod_discp=2
+	  -- utilizando atálhos
